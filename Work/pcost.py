e588ff6a91ec40cf78639c7968c2affd3a65e279
Michael-Tzaperas <Michael.Tzaperas@enverus.com>
2021-08-12 16:52:56 -0500

    change pcost to function

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,11 +2,17 @@
 #
 # Exercise 1.27
 
-with open('Data/portfolio.csv','rt') as portfolio:
-	cost = 0
-	file_header = next(portfolio)
-	for stock in portfolio:
-		name, num_shares, purchase_price = stock.split(',')
-		cost += int(num_shares) * float(purchase_price)
+def portfolio_cost(filename):
+	'Calculates purchase cost of portfolio from csv file'
 
-print(f'Total cost {round(cost,2)}')
+	with open(filename,'rt') as portfolio:
+		cost = 0
+		file_header = next(portfolio)
+		for stock in portfolio:
+			name, num_shares, purchase_price = stock.split(',')
+			cost += int(num_shares) * float(purchase_price)
+	
+	return round(cost,2)
+
+cost = portfolio_cost('Data/portfolio.csv')
+print(f'Total cost {cost}')
