5af1f4a6102658aa0abd6beba6a5a3a2cb085b9d
Michael-Tzaperas <Michael.Tzaperas@enverus.com>
2021-08-12 16:36:02 -0500

    write pcost

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+
+with open('Data/portfolio.csv','rt') as portfolio:
+	cost = 0
+	file_header = next(portfolio)
+	for stock in portfolio:
+		name, num_shares, purchase_price = stock.split(',')
+		cost += int(num_shares) * float(purchase_price)
+
+print(f'Total cost {round(cost,2)}')
