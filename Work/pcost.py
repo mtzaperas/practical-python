5b42c82cde77ace3ae3f62cd667ec1ac0c3e2d5b
Michael-Tzaperas <Michael.Tzaperas@enverus.com>
2021-08-13 06:34:53 -0500

    pcost import csv

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,16 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
 
 def portfolio_cost(filename):
 	'Calculates purchase cost of portfolio from csv file'
-
 	with open(filename,'rt') as portfolio:
 		cost = 0
+		stocks = csv.reader(portfolio)
 		file_header = next(portfolio)
-		for stock in portfolio:
+		for stock in stocks:
 			try:
-				name, num_shares, purchase_price = stock.split(',')
+				name, num_shares, purchase_price = stock
 				cost += int(num_shares) * float(purchase_price)
 			except ValueError:
 				print('Couldnt parse: ', stock)
